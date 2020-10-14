1bf30406f301e4edb2d3da74dfabe3298241bc98
Rajiv Malhotra <malhotra.rajiv@principal.com>
2020-10-14 14:13:27 -0500

    Set Assignment - Added logic to in_set function

--- more_fun_with_collections/set_membership.py
+++ more_fun_with_collections/set_membership.py
@@ -8,8 +8,15 @@ Set Assignment
 
 
 def in_set(some_set, some_value):
-    pass
+    """
+    Function accepts a set and returns a boolean value stating if the element is in the set.
+    :param some_set: This is a set
+    :param some_value: This is a string
+    :return: Boolean - True or False
+    """
+    return some_value in some_set
 
 
 if __name__ == '__main__':
     my_set = set('0123456789')
+    print("Is {} in {}? {}".format(5, my_set, in_set(my_set, str(5))))
